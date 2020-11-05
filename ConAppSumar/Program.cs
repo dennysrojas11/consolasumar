91e12805319556db0b71d569a36a203c0b4fcab2
Dennys Rojas <65422210+dennysrojas11@users.noreply.github.com>
2020-11-04 20:24:39 -0500

    Carga del proyecto
    Carga del proyecto suma por consola

---
+++ ConAppSumar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConAppSumar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ud ingreso un número de parámetros {0}", args.Length);
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Debe ingresar únicamente dos (2) valores");
+            }
+            else
+            {
+                int numero1 = int.Parse(args[0]);
+                int numero2 = int.Parse(args[1]);
+                int resultadoSuma = numero1 + numero2;
+                Console.WriteLine("La suma es: {0}", resultadoSuma);
+            }
+        }
+    }
+}
